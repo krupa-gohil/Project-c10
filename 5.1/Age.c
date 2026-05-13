844724c72ed382791ea7cfc3e92931ec9fde9902
Krupa Gohil <krupagohil2910@gmail.com>
2026-05-13 17:17:09 +0530

    Add files via upload

---
+++ 5.1/Age.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+main()
+{
+	
+	int age =18;
+	printf ("Enter your age : " );
+	scanf ("%d",&age);
+				
+	if (age >= 18)
+{
+	
+	printf("\n\n");
+	printf("you can vote");
+}
+
+     else
+ {
+	printf("\n\n");
+	printf("you can not vote");
+}
+	
+
+}
---
+++ 5.1/number.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+main()
+{
+	int first, second;
+	
+	printf("Enter a value of the first namber :");
+	scanf("%d", &first);
+	printf("\n\n";)
+	printf("Enter a value of the second number :");
+	sacnf("%d", &second);
+	
+	if(frist < second)
+	printf("The minimum value is :%d\n",first);
+	
+	else
+	{
+		printf("The minimum value is :%d\n",second);
+		}	
+	
+	
+		
+	
+}
+	
