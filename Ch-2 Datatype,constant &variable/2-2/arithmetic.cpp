1fb4d5b5013a1d0cdeb345a7d168ef72a35c6b81
Krupa Gohil <krupagohil2910@gmail.com>
2026-05-14 17:22:51 +0530

    Add files via upload

---
+++ Ch-2 Datatype,constant &variable/2-2/arithmetic.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+main()
+{
+	int a= 50, b=4;
+	
+	printf("Addition of %d and %d ans is %d\n ",a,b,a+b);
+	printf("subtraction of %d and %d ans is %d\n\n",a,b,a-b);
+	
+	printf("%d + %d = %d\n",a,b,a-b);
+	printf("%d * %d = %d",a,b,a*b);
+}
---
+++ Ch-2 Datatype,constant &variable/2-3/area of triangle.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+()
+{
+	float base, height;
+	
+	printf("Enter base :");
+	scanf("%F",&base);
+	printf("Enter height :");
+	scanf("%F",&height);
+	
+	printf("area of triangle :%.2f",05*base*height);
+}
