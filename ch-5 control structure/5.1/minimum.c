6b361ebaac9d6d7a4f3e339f2d04bc701d4c1907
Krupa Gohil <krupagohil2910@gmail.com>
2026-05-14 16:58:59 +0530

    Add files via upload

---
+++ ch-5 control structure/5.1/minimum.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+main()
+{
+	int a,b;
+	
+	printf("Enter first number :");
+	scanf("%d", &a);
+	printf("Enter second number : ");
+	scanf("%d", &b);
+	 
+	(a < b) ? printf("%d is minimum" , a) : printf("%d is minimum",b);
+}
+		
+	
+	
+    
+	
+	
+	
