be13e7ca1cf67ded771e14270f05d4c1a805bc5c
Krupa Gohil <krupagohil2910@gmail.com>
2026-05-19 12:12:10 +0530

    Add files via upload

---
+++ project-1.c/celsius.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	float celsius,
+	fahrenheit;
+	
+	printf("Enter temperature in celsius :");
+	scanf("%f",&celsius); 
+	printf("fahrenheit: %.2f\n",fahrenheit);
+
+	
+}
